141325af8fc6dbd95e003076d2d025f4d59406af
Prerit <preritmishra2062670@gmail.com>
2024-07-26 19:17:12 +0000

    Added Category table

---
+++ src/main/java/com/inventorymanagement/category/Category.java
@@ -0,0 +1,22 @@
+package com.inventorymanagement.category;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long categoryId;
+	
+	private String categoryName;
+}
---
+++ src/main/java/com/inventorymanagement/category/CategoryController.java
@@ -0,0 +1,40 @@
+package com.inventorymanagement.category;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1.0/categories")
+public class CategoryController {
+	
+	@Autowired
+	CategoryService service;
+
+	@GetMapping("/all")
+	Iterable<Category> getAllCategories() {
+		return service.getAllCategories();
+	}
+	
+	@GetMapping("/all/{id}")
+	Optional<Category> getCategoryById(@PathVariable Long id) {
+		return service.getCategoryById(id);
+	}
+	
+	@PostMapping("/all")
+	void addNewCategory(@RequestBody Category category) {
+		service.addNewCategory(category);
+	}
+	
+	@DeleteMapping("/all/{id}")
+	void deleteACategoryById(@PathVariable Long id) {
+		service.deleteACategoryById(id);
+	}
+}
