7e424d8f3ca6b63d2cc3443a7208cfc8526e4a76
Prerit <prerit1913059@akgec.ac.in>
2024-07-29 00:23:26 +0530

    Added SalesOrder Table

--- src/main/java/com/inventorymanagement/salesOrders/SalesOrder.java
+++ src/main/java/com/inventorymanagement/salesOrders/SalesOrder.java
@@ -2,24 +2,43 @@ package com.inventorymanagement.salesOrders;
 
 import java.sql.Timestamp;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.inventorymanagement.products.Product;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//@Entity
+@Entity
 @Data
 @NoArgsConstructor
 public class SalesOrder {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
+	private Long salesOrderId;
 	
-	private int quantity;
+	private Integer quantity;
 	
+	@CreationTimestamp
+	@Column(nullable=false, updatable=false)
 	private Timestamp orderDate;
+	
+	@ManyToOne
+	private Product product;
+
+	public SalesOrder(Long salesOrderId, Integer quantity,Timestamp orderDate, Long productId) {
+		this.salesOrderId = salesOrderId;
+		this.quantity = quantity;
+		this.orderDate=orderDate;
+		this.product = new Product(productId);
+	}
+	
 	
 	
 }
--- src/main/java/com/inventorymanagement/salesOrders/SalesOrderService.java
+++ src/main/java/com/inventorymanagement/salesOrders/SalesOrderService.java
@@ -1,11 +1,48 @@
 package com.inventorymanagement.salesOrders;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.inventorymanagement.products.Product;
 
 @Service
 public class SalesOrderService {
 
-//	@Autowired
-//	SalesOrderRepository repo;
+	@Autowired
+	SalesOrderRepository repo;
+
+	Iterable<SalesOrder> getAllSalesOrders() {
+		return repo.findAll();
+	}
+	
+	Optional<SalesOrder> getSalesOrderById(Long id) {
+		return repo.findById(id);
+	}
+	
+	List<SalesOrder> getAllSalesOrdersByProductId(Long productId) {
+		return repo.findByProductProductId(productId);
+	}
+
+	void addNewSalesOrder(SalesOrder salesOrder) {
+		repo.save(salesOrder);
+	}
+
+	void updateSalesOrder(SalesOrder salesOrder) {
+		repo.save(salesOrder);
+	}
+
+	void deleteASalesOrderById(Long id) {
+		repo.deleteById(id);
+	}
+
+	@Transactional
+	void deleteAllSalesOrderByProductId(Long productId) {
+		repo.deleteAllSalesOrderByProductId(productId);
+	}
+
+
 }
