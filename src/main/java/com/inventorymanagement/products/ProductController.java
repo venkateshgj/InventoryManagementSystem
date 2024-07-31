e3135142a18b38cc0481504d905f8c5a0811a8f9
labuser <deepupradeep232@gmail.com>
2024-07-31 04:30:33 +0000

    changes in product controller

--- src/main/java/com/inventorymanagement/products/ProductController.java
+++ src/main/java/com/inventorymanagement/products/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.inventorymanagement.categories.Category;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1.0/products")
 public class ProductController {
 
